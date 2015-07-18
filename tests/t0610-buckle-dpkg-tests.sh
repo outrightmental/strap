fda84387005d47231644896508b12875a0e86146
Nick C. Kaye <nick.c.kaye@gmail.com>
2015-07-18 12:00:02 -0400

    smoke test for implementing a dpkg buckle

--- tests/t0610-buckle-dpkg-tests.sh
+++ tests/t0610-buckle-dpkg-tests.sh
@@ -4,7 +4,13 @@ test_description='Test buckle type: dpkg'
 cd "$(dirname "$0")"
 . ./setup.sh
 
-# TODO: smoke test for implementing a dpkg buckle
+test_expect_success 'Test buckles of type "dpkg"' '
+  "$STRAP" init &&
+  "$STRAP" insert -fm "config"<<<"straps:\n  ubuntu: [[ \"\$OSTYPE\" == \"linux\"* ]] && apt-get -v" &&
+  "$STRAP" insert -e "ubuntu/passwordstore"<<<"type: dpkg\nname: git" &&
+  "$STRAP"
+'
+
 # TODO: test that buckle passes quietly if package is installed
 # TODO: test that buckle installs package if not currently listed as "installed" in dpkg -s
 
