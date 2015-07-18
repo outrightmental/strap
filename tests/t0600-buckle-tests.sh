1b1537045fc2971c9918248605e81d8822e9248a
Nick C. Kaye <nick.c.kaye@gmail.com>
2015-07-18 11:52:52 -0400

    test 0600 smoke test strap command

--- tests/t0600-buckle-tests.sh
+++ tests/t0600-buckle-tests.sh
@@ -4,6 +4,11 @@ test_description='Test buckle prototype'
 cd "$(dirname "$0")"
 . ./setup.sh
 
-# TODO: smoke test for implementing any buckle_xxxxxx function based on the type: listed in a buckle .sh.yml file
+test_expect_success 'Test "strap" command' '
+  "$STRAP" init &&
+  "$STRAP" insert -fm "config"<<<"straps:\n  base: echo" &&
+  "$STRAP" insert -e "base/passwordstore"<<<"type: pass" &&
+  "$STRAP"
+'
 
 test_done
