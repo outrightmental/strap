93c3d14ee9e1cb6b75f8060a5ff1a3bb93ad877c
Nick C. Kaye <nick.c.kaye@gmail.com>
2015-07-18 12:02:30 -0400

    test buckles of type pass

--- tests/t0611-buckle-pass-tests.sh
+++ tests/t0611-buckle-pass-tests.sh
@@ -4,9 +4,14 @@ test_description='Test buckle type: pass'
 cd "$(dirname "$0")"
 . ./setup.sh
 
+test_expect_success 'Test buckles of type "pass"' '
+  "$STRAP" init &&
+  "$STRAP" insert -fm "config"<<<"straps:\n  base: echo" &&
+  "$STRAP" insert -e "base/passwordstore"<<<"type: pass" &&
+  "$STRAP"
+'
+
 # TODO: test configuring a new pass
 # TODO: test updating an existing pass
-
-# type: pass
 
 test_done
