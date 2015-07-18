c1cb88b0ec7715ff5ced4504d378f56f7d7becab
Nick C. Kaye <nick.c.kaye@gmail.com>
2015-07-18 12:10:36 -0400

    test buckle of type repo

--- tests/t0612-buckle-repo-tests.sh
+++ tests/t0612-buckle-repo-tests.sh
@@ -6,13 +6,19 @@ cd "$(dirname "$0")"
 
 test_expect_success 'Can strap buckle of type "repo"' '
   "$STRAP" init &&
-  "$STRAP" insert -fm "config"<<<"straps:\n  gh: ping -c1 github.com" &&
-  "$STRAP" insert -e "gh/strap"<<<"type: repo\nvcs: git\nurl: git@github.com/outrightmental/strap.git\npath: \$HOME/OpenSource/strap" &&
+  "$STRAP" insert -fm "config"<<<"straps:\n  base: echo" &&
+  "$STRAP" insert -e "base/rbenv"<<<"type: rbenv" &&
   "$STRAP"
 '
 
 # TODO: test configuring a new rpo
 # TODO: test updating an existing repo
 # TODO: some kind of mock git repo harness
+
+# type: repo
+# vcs: git
+# url: git@github.com:charneykaye/pass.git
+# clone_as: .password-store
+# parent_path: $HOME
 
 test_done
