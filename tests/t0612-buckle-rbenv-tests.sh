bc1b8288e6ffb55d33badc1b38207cf5228d5c9c
Nick C. Kaye <nick.c.kaye@gmail.com>
2015-07-18 12:04:14 -0400

    test buckle of type rbenv

--- tests/t0612-buckle-rbenv-tests.sh
+++ tests/t0612-buckle-rbenv-tests.sh
@@ -4,6 +4,13 @@ test_description='Test buckle type: rbenv'
 cd "$(dirname "$0")"
 . ./setup.sh
 
+test_expect_success 'Test buckles of type "rbenv"' '
+  "$STRAP" init &&
+  "$STRAP" insert -fm "config"<<<"straps:\n  base: echo" &&
+  "$STRAP" insert -e "base/rbenv"<<<"type: rbenv" &&
+  "$STRAP"
+'
+
 # TODO: test configuring a new rbenv
 # TODO: test updating an existing rbenv
 
